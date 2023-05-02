ed8f98b049d89d3fa1238b667bab800a77e423a2
Kirill Aliyev <kirill.linas@gmail.com>
2023-05-02 19:19:19 +0400

    added docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def cap_string(string):
+    """Функция которая возвращает слово в большом регистре"""
     return string.upper()
+
+
+def cap_title(input_string):
+    """Функция которая возвращает каждую первую букву в слове большой"""
+    return input_string.title()
+
